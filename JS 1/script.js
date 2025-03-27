55277344b3959d522cafbb6346dc4c8455a7ee7c
Elisangela Vieira <elyvieira95@gmail.com>
2025-03-27 00:22:32 -0300

    primeiro commit

---
+++ JS 1/script.js
@@ -0,0 +1,19 @@
+let botao = document.querySelector("#botao");
+botao.style.background= "blue"
+
+botao.addEventListener("mouseover",e=>{
+        botao.style.background= "green"
+    });
+
+    botao.addEventListener("mouseout",e=>{
+        botao.style.background= "blue"
+    });
+
+    botao.addEventListener("click",e=>{
+        botao.style.background= "red"
+        botao.innerHTML="Quebrei"
+    });
+
+
+
+
